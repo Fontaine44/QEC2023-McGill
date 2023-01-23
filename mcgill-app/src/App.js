e67785c1dba21950aadcebe7ad4833d885301c25
Raphaël Fontaine <raphael.fontaine@mail.mcgill.ca>
2023-01-23 13:59:00 -0500

    Add react-bootstrap and fix navbar

--- mcgill-app/src/App.js
+++ mcgill-app/src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Page2 from './pages/Page2'
-import Navbar from './components/Navbar'
+import NavBar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path='/' element={< Home />}></Route>
         <Route path='/page2' element={< Page2 />}></Route>
--- mcgill-app/src/components/Navbar.js
+++ mcgill-app/src/components/Navbar.js
@@ -1,35 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
-const Navbar = () => {
+const NavBar = () => {
  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand ps-2" to="/">McGill</Link>
-      
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/page2">Page2</Link>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <Link className="dropdown-item" to="/">Home</Link>
-              <Link className="dropdown-item" to="/page2">Home</Link>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  <Navbar bg="light" expand="lg">
+    <Container className="ms-0">
+      <Navbar.Brand>McGill</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/page2">
+            <Nav.Link>Page 2</Nav.Link>
+          </LinkContainer>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <LinkContainer to="/home">
+             <NavDropdown.Item>Home</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/page2">
+             <NavDropdown.Item>Page 2</NavDropdown.Item>
+            </LinkContainer>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 };
 
-export default Navbar;
+export default NavBar;
