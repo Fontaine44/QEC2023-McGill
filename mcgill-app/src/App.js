70f9aa8b572e4cbb45f208472cef9a4972b18c4c
Raphaël Fontaine <raphael.fontaine@mail.mcgill.ca>
2023-01-23 12:42:34 -0500

    Add Router

--- mcgill-app/src/App.js
+++ mcgill-app/src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './pages/Home'
+import Page2 from './pages/Page2'
 
 function App() {
   return (
-    <div className="App">
-      Hello World <br/>
-      <button type="button" class="btn btn-primary">Primary</button>
-    </div>
+    <>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page2" >Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path='/' element={< Home />}></Route>
+        <Route path='/page2' element={< Page2 />}></Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
