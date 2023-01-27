4602af9187fcd3332c98d54374c6683be684814d
C-APare <charlotpare@hotmail.ca>
2023-01-26 23:12:24 -0500

    create new branch

--- backend-api/users.py
+++ backend-api/users.py
@@ -1,7 +1,27 @@
 import database
 
-def login(username):
-    query = {"id": username}
-
-    return database.database_find(query, "users")
+def login(data):
+    query = {"id": data["id"]}
+    list = database.database_find(query, "users")
+    if len(list) == 0:
+        return False, None
+    else:
+        for user in list:
+            if user["password"] == data["password"]:
+                return True, user
+            else:
+                return False, None
+            
+def set_password(data):
+    filter = {"id": data["id"]}
+    newvalues = {"$set": { "password": data["password"] }}
+    database.update(filter, newvalues, "users")
+    
+def create_user(data):
+    mydict = {"id": data["id"], "password": data["password"], "firstname": data["firstname"], "lastname": data["lastname"], "age": data["age"], "firstlogin": True, "type": data["type"]}
+    database.insert(mydict, "users")
+    
+def get_users():
+    return database.database_find({}, "users")
+    
 
