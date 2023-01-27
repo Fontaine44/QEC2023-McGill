cfd8663d27d62ffc240cf1bb73894bf4012de45e
C-APare <charlotpare@hotmail.ca>
2023-01-26 23:41:33 -0500

    tasks service

--- backend-api/tasks.py
+++ backend-api/tasks.py
@@ -2,12 +2,15 @@ import database
 
 
 def create_task(data):
-    pass
+    mydict = {"name": data["name"], "description": data["description"],"date": data["date"],"startTime": data["endTime"],"userId": data["userId"]}
+    database.insert(mydict, "tasks")
 
-
-def update_task(data):
-    pass
+def update_task(data, new_data):
+    filter = {"name": data["name"], "description": data["description"],"date": data["date"],"startTime": data["endTime"],"userId": data["userId"]}
+    newvalue = {"name": new_data["name"], "description": new_data["description"],"date": new_data["date"],"startTime": new_data["endTime"],"userId": new_data["userId"]}
+    database.update(filter, newvalue, "tasks")
 
 
 def delete_task(data):
-    pass
+    query = {"name": data["name"], "description": data["description"],"date": data["date"],"startTime": data["endTime"],"userId": data["userId"]}
+    database.delete(query, "tasks")
