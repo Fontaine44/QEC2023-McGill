49f9b4618aea2c2dbf569e88bfd87c259f29b987
C-APare <charlotpare@hotmail.ca>
2023-01-26 23:48:48 -0500

    problems services

--- backend-api/problems.py
+++ backend-api/problems.py
@@ -2,8 +2,8 @@ import database
 
 
 def create_problem(data):
-    pass
+    mydict = {"name": data["name"],"description": data["description"],"assistance_type": data["assistance_type"]}
 
 
 def get_problems():
-    pass
+    return database.database_find({}, "reports")
