7730dd9a44c7907cebb5ee794327bbfa209c6eb7
C-APare <charlotpare@hotmail.ca>
2023-01-27 00:13:26 -0500

    set$

--- backend-api/tasks.py
+++ backend-api/tasks.py
@@ -7,7 +7,7 @@ def create_task(data):
 
 def update_task(data, new_data):
     filter = {"name": data["name"], "description": data["description"],"date": data["date"],"startTime": data["endTime"],"userId": data["userId"]}
-    newvalue = {"name": new_data["name"], "description": new_data["description"],"date": new_data["date"],"startTime": new_data["endTime"],"userId": new_data["userId"]}
+    newvalue = {"$set": {"name": new_data["name"], "description": new_data["description"],"date": new_data["date"],"startTime": new_data["endTime"],"userId": new_data["userId"]}}
     database.update(filter, newvalue, "tasks")
 
 
