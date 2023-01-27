5508c5faa89379f8552ae0f73526a9984099d981
C-APare <charlotpare@hotmail.ca>
2023-01-27 01:55:24 -0500

    backend is cool

--- backend-api/problems.py
+++ backend-api/problems.py
@@ -2,30 +2,25 @@ import database, users
 from twilio.rest import Client
 
 
+
 def create_problem(data):
     list = database.database_find(data, "reports")
     if len(list)>0:
         return False
-    database.insert(data, "reports")
+    
     if data["priority"] == 1:
         account_sid = "AC7ea49dde3a5ccc0beecd02dc51ffebad"
         auth_token = "d3b4afe1425bb4aeb9aaec8953215d2b"
         client = Client(account_sid, auth_token)
-        list = users.get_users()
+        list = database.database_find({"phone": {"$exists": "true"}}, "users")
         for user in list:
             message = client.messages.create(
-            body=data["name"],
+            body=data["name"]+ '!: '+ data["description"],
             from_="+15644647446",
             to=user["phone"]
             )
             message.sid
-            message = client.messages.create(
-            body=data["description"],
-            from_="+15644647446",
-            to=user["phone"]
-            )
-            message.sid
-            
+    database.insert(data, "reports")
     return True
 
 
--- backend-api/users.py
+++ backend-api/users.py
@@ -1,4 +1,6 @@
 import database
+import smtplib
+from email.message import EmailMessage
 
 def login(data):
     query = {"id": data["id"]}
@@ -21,6 +23,16 @@ def create_user(data):
     list = database.database_find(data, "users")
     if len(list)>0:
         return False
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('FlackEvenTask@gmail.com', 'aqppxqoobrdlgbte')
+    msg = EmailMessage()
+    msg.set_content('Hello '+data["firstname"]+' '+data["lastname"]+', \nCreate your EvenTask account at "https://www.google.com/". \nUsername: '+data["id"]+ ' \nTemporary password: '+data["password"]) #Sujet du email
+    msg['Subject'] = 'Create EvenTask account' #Email Subject
+    msg['From'] = 'FlackEvenTask@gmail.com'
+    msg['To'] = data["address"]
+
+    server.send_message(msg)
     database.insert(data, "users")
     return True
     
