10623030669801f0b2544b86c2021613b3bc3ec0
C-APare <charlotpare@hotmail.ca>
2023-01-26 23:26:09 -0500

    objects service

--- backend-api/objs.py
+++ backend-api/objs.py
@@ -2,16 +2,17 @@ import database
 
 
 def get_lost():
-    pass
+    return database.database_find({"type": "LOST"}, "objects")
 
 
 def get_found():
-    pass
+    return database.database_find({"type": "FOUND"}, "objects")
 
 
-def create_object():
-    pass
+def create_object(data):
+    mydict = {"name": data["name"],"description": data["description"],"find_hour": data["find_hour"],"type": data["type"]}
+    database.insert(mydict, "objects")
 
-
-def delete_object():
-    pass
+def delete_object(data):
+    query = {"name": data["name"], "description": data["description"],"find_hour": data["find_hour"],"type": data["type"]}
+    database.delete(query, "objects")
